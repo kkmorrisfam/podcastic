e1e7af9ee5c83cc244ddf7476a6cac9b497ef936
Erik Burton <erikburton1@gmail.com>
2025-11-28 20:16:27 -0700

    Create User model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const EpisodeSchema = new mongoose.Schema(
+  {
+    id: String,
+    title: String,
+    audioUrl: String,
+    podcastId: Number,
+    image: String,
+    author: String,
+    durationSec: Number,
+    publishedAt: Number,
+    episode: Number,
+    feedImage: String,
+  },
+  { _id: false }
+);
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+
+    library: {
+      type: Map,
+      of: EpisodeSchema,
+      default: {},
+    },
+
+    favorites: {
+      type: [String],
+      default: [],
+    },
+
+    queue: {
+      type: [
+        {
+          episodeId: String,
+        },
+      ],
+      default: [],
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
